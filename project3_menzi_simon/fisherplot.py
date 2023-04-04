2a74d452d75f4ae1bd724b062774f7519f5be70e
amenzi <amenzi@student.ethz.ch>
2023-04-04 10:24:29 +0200

    changed the plotting a bit

--- project3_menzi_simon/fisherplot.py
+++ project3_menzi_simon/fisherplot.py
@@ -1,9 +1,9 @@
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
 
-df = pd.read_csv("runtimes.csv")
+df = pd.read_csv("strong_output.csv")
 
 # Strong scaling plot
 runtimes = df["runtime"]
@@ -20,13 +20,13 @@ for n in sizes:
     print(speedup)
     plt.plot(threads, speedup, label=n)
 
-    if max_speedup < speedup.max(): 
+    if max_speedup < speedup.max():
         max_speedup = speedup.max()
 
-pyplot.plt.xlabel('Number of Threads')
-pyplot.plt.ylabel('Speedup')
-pyplot.plt.legend()
+plt.xlabel('Number of Threads')
+plt.ylabel('Speedup')
+plt.legend()
 
-pyplot.plt.plot(np.array([1, max_speedup]), np.array([1, max_speedup]), 'r--')
+plt.plot(np.array([1, max_speedup]), np.array([1, max_speedup]), 'r--')
 
 plt.savefig('strong_scaling.png')
