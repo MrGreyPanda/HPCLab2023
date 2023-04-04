e01132ab59a7fb4b3964b80ccc3effa892dd5c3c
amenzi <amenzi@student.ethz.ch>
2023-04-04 18:55:22 +0200

    Strong and weak Scaling done

--- project3_menzi_simon/fisherplot.py
+++ project3_menzi_simon/fisherplot.py
@@ -10,6 +10,7 @@ df['runtime_avg'] = df.iloc[:, 2:].mean(axis=1)
 # Store the result in a variable called runtimes
 runtimes = df['runtime_avg']
 
+
 plt.title('Strong Scaling')
 
 max_speedup = 0
@@ -29,3 +30,34 @@ plt.ylabel('Speedup')
 plt.legend()
 plt.plot(np.array([1, max_speedup]), np.array([1, max_speedup]), 'r--')
 plt.savefig('strong_scaling.png')
+
+
+plt.clf()  # Clear plot
+
+plt.title("Weak Scaling")
+
+
+efficiencies = np.array([1])
+weak_scaling_threads = np.array([1])
+thread_number = 2
+for i, n in enumerate(sizes[-13:]):
+    if thread_number > threads.max():
+        break
+
+    serial_runtime = df[df["size"] == n]["runtime_avg"].iloc[0]
+    parallel_runtime = df[(df["size"] == n) & (
+        df["threads"] == thread_number)]["runtime_avg"].iloc[0]
+    speedup = serial_runtime / parallel_runtime
+    efficiencies = np.append(efficiencies, speedup / thread_number)
+    weak_scaling_threads = np.append(weak_scaling_threads, thread_number)
+    thread_number *= 2
+
+plt.plot(weak_scaling_threads, efficiencies)
+plt.plot(np.array([weak_scaling_threads.min(),
+         weak_scaling_threads.max()]), np.array([1, 1]), 'r--')
+
+plt.ylim(0, np.max([1.05, efficiencies.max() + 0.05]))
+plt.xlabel('Number of Threads')
+plt.ylabel('Efficiency')
+
+plt.savefig('weak_scaling.png')
