acac08a2b9f72bbe8f7fff10c15e94843a441a58
Aaron Menzi <amenzi@eu-login-47.euler.ethz.ch>
2023-04-04 08:34:48 +0200

    Some more plots

--- project3_menzi_simon/run.sh
+++ project3_menzi_simon/run.sh
@@ -8,13 +8,28 @@ module load gcc python
 # Run makefile
 make
 
-for problemsize in 128 256 512 1028
+# Timeings
+for problemsize in 64 128 256 512 1024
 do
     for threads in 1 2 4 8 16 24 
     do
         export OMP_NUM_THREADS=$threads
-        ./main $problemsize 150 0.005 >> stat_output.csv
+        ./main $problemsize 100 0.005 >> stat_output.csv
     done
+    echo >> stat_output.csv
 done
+
+#for speedup
+# for threads in 1 2 4 8 16 24 
+# do
+#     for problemsize in 64 128 256 512 1024
+#     do
+#         export OMP_NUM_THREADS=$threads
+#         ./main $problemsize 100 0.005 >> stat_output.csv
+#     done
+#     echo >> stat_output.csv
+#     echo $threads, >> stat_output
+# done
+
 
 make clean
