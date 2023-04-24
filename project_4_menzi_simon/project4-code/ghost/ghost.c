ac6d7c19439301e3d820d1900627d85d6be6b519
amenzi <amenzi@student.ethz.ch>
2023-04-24 10:07:13 +0200

    modfied ghost.c

--- project_4_menzi_simon/project4-code/ghost/ghost.c
+++ project_4_menzi_simon/project4-code/ghost/ghost.c
@@ -56,10 +56,10 @@ int main(int argc, char *argv[])
 {
     int rank, size, i, j, dims[2], periods[2], rank_top, rank_bottom, rank_left, rank_right;
     double data[DOMAINSIZE*DOMAINSIZE];
-    MPI_Request request;
+    MPI_Request send_request[4], recv_request[4];
     MPI_Status status;
     MPI_Comm comm_cart;
-    MPI_Datatype data_ghost_col, data_ghost_row;
+    MPI_Datatype data_ghost;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -82,14 +82,14 @@ int main(int argc, char *argv[])
     int dim = 4;
     dims[0]= dim;
     dims[1]= dim;
-    periods[0]= true;
-    periods[1]= true;
+    periods[0]= 1;
+    periods[1]= 1;
 
     // TODO: Create a Cartesian communicator (4*4) with periodic boundaries (we do not allow
     // the reordering of ranks) and use it to find your neighboring
     // ranks in all dimensions in a cyclic manner.
     MPI_Comm cart_comm;
-    MPI_Cart_create(MPI_COMM_WORLD, 2, dims, periods, true, &cart_comm);
+    MPI_Cart_create(MPI_COMM_WORLD, 2, dims, periods, 1, &cart_comm);
     MPI_Comm_rank(cart_comm, &rank);
     // TODO: find your top/bottom/left/right neighbor using the new communicator, see MPI_Cart_shift()
     MPI_Cart_shift(cart_comm, 0, 1, &rank_left, &rank_right);
@@ -97,26 +97,31 @@ int main(int argc, char *argv[])
 
     //  TODO: create derived datatype data_ghost, create a datatype for sending the column, see MPI_Type_vector() and MPI_Type_commit()
     // data_ghost
-    MPI_Type_contiguous(dim, MPI_DOUBLE, &data_ghost_row);
-    MPI_Type_commit(&data_ghost_row);
-    MPI_Type_vector(dim, DOMAINSIZE, dim, MPI_DOUBLE, &data_ghost_col);
-    MPI_Type_commit(&data_ghost_col);
+    MPI_Type_vector(SUBDOMAIN, 1, DOMAINSIZE, MPI_DOUBLE, &data_ghost);
+    MPI_Type_commit(&data_ghost);
 
     //  TODO: ghost cell exchange with the neighbouring cells in all directions
     //  use MPI_Irecv(), MPI_Send(), MPI_Wait() or other viable alternatives
 
-    //  to the top
-    MPI_Sendrecv(&data[DOMAINSIZE + 1], SUBDOMAIN, data_ghost_row, rank_top, 0, &data[1], SUBDOMAIN, data_ghost_row, rank_top, 0, comm_cart, status);
-    
-    //  to the bottom
-    MPI_Sendrecv(&data[DOMAINSIZE * (DOMAINSIZE - 2) + 1], SUBDOMAIN, data_ghost_row, rank_bottom, 0, &data[DOMAINSIZE * (DOMAINSIZE - 1) + 1], SUBDOMAIN, data_ghost_row, rank_top, 0, comm_cart, status);
+    // top
+    MPI_Irecv(&data[1], SUBDOMAIN, MPI_DOUBLE, rank_top, 0, cart_comm, &recieve_request[0]);
+    MPI_Isend(&data[DOMAINSIZE + 1], SUBDOMAIN, MPI_DOUBLE, rank_top, 0, cart_comm, send_request[0]);
 
-    //  to the left
-    MPI_Sendrecv(&data[DOMAINSIZE + 1], SUBDOMAIN, data_ghost_col, rank_left, 0, &data[DOMAINSIZE], SUBDOMAIN, data_ghost_col, rank_left, 0, comm_cart, status);
-   
-    //  to the right
-    MPI_Sendrecv(&data[DOMAINSIZE + SUBDOMAIN], SUBDOMAIN, data_ghost_col, rank_right, 0, &data[DOMAINSIZE + SUBDOMAIN + 1], SUBDOMAIN, data_ghost_col, rank_right, 0, comm_cart, status);
-    
+    // bottom
+    MPI_Irecv(&data[DOMAINSIZE * (DOMAINSIZE - 1) + 1], SUBDOMAIN, MPI_DOUBLE, rank_bottom, 0, cart_comm, &recieve_request[1]);
+    MPI_Isend(&data[DOMAINSIZE * (DOMAINSIZE - 2) + 1], SUBDOMAIN, MPI_DOUBLE, rank_bottom, 0, cart_comm, send_request[1]);
+
+    // left
+    MPI_Irecv(&data[DOMAINSIZE], 1, data_ghost, rank_left, 0, cart_comm, &recieve_request[2]);
+    MPI_Isend(&data[DOMAINSIZE + 1], 1, data_ghost, rank_left, 0, cart_comm, send_request[2]);
+
+    // right
+    MPI_Irecv(&data[DOMAINSIZE * 2 - 1], 1, data_ghost, rank_right, 0, cart_comm, &recieve_request[3]);
+    MPI_Isend(&data[DOMAINSIZE * 2 - 2], 1, data_ghost, rank_right, 0, cart_comm, send_request[3]);
+
+
+    MPI_Waitall(4, send_request, status);
+    MPI_Waitall(4, recieve_request, status);
 
     if (rank==9) {
         printf("data of rank 9 after communication\n");
@@ -129,8 +134,7 @@ int main(int argc, char *argv[])
     }
 
     // TODO: uncomment when done with tasks above
-    MPI_Type_free(&data_ghost_col);
-    MPI_Type_free(&data_ghost_row);
+    MPI_Type_free(&data_ghost);
     MPI_Comm_free(&comm_cart);
     MPI_Finalize();
 
