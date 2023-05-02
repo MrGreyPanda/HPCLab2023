f29ce69781ac0d447f45ac605ca242a454495b17
amenzi <amenzi@student.ethz.ch>
2023-05-02 13:06:41 +0200

    Lets see if Phind is correct

--- project_5_menzi_simon/project5-code/Tools/read_csv_graph.jl
+++ project_5_menzi_simon/project5-code/Tools/read_csv_graph.jl
@@ -1,5 +1,18 @@
 #   M.L. for High Performance Computing Lab @USI & @ETHZ - malik.lechekhab@usi.ch 
 #   Data extraction method
+function create_adjacency_matrix(data::Matrix{Float64})
+    n = maximum(data)
+    A = zeros(Int64, n, n)
+
+    for (i, j) in eachrow(data)
+        A[Int64(i), Int64(j)] = 1
+        A[Int64(j), Int64(i)] = 1
+    end
+
+    return A
+end
+
+
 """
     read_csv_graph(path_file)
 
@@ -9,12 +22,18 @@ function read_csv_graph(path_file)
     # Steps
     #   1.  Load the .csv files
     #       see readdlm(...)
+    data = readdlm(path_file, ',', Float64)
+    path_coords = replace(path_file, "-adj.csv", "-pts.csv")
+    coords = readdlm(path_coords, ',', Float64)
 
     #   2.  Construct the adjacency matrix A
+    A = create_adjacency_matrix(data)
 
     #   3.  Visualize and save the result 
     #       use drawGraph(A, coords)
-
+    drawGraph(A, coords)
+    
     #   4.  Return the matrix A and the coordinates 
     #       return(A, coords)
+    return(A, coords)
 end
